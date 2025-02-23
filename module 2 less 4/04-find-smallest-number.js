38c14521ec142be0909c69cfa169f85ec757a883
Nazar <nazarcorovol09@gmail.com>
2025-02-23 19:00:56 +0200

    new js

--- module 2 less 4/04-find-smallest-number.js
+++ module 2 less 4/04-find-smallest-number.js
@@ -1,10 +1,1 @@
-const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz'];
 
-const findLogin = function (allLogins, loginToFind) {
-  console.log(allLogins);
-};
-
-console.log(findLogin(logins, 'robotGoogles'));
-console.log(findLogin(logins, 'Poly'));
-console.log(findLogin(logins, 'Mango'));
-console.log(findLogin(logins, 'Aj4x1sBozz'));
