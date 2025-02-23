770362a4cdf85d61bf9b331635ff0397bb280295
Nazar <nazarcorovol09@gmail.com>
2025-02-23 18:56:02 +0200

    js

---
+++ module 2 less 4/01-basics.js
@@ -0,0 +1,46 @@
+// const add = function (a, b) {
+//   console.log(a);
+//   console.log(b);
+
+//   const result = a + b;
+
+//   console.log('Done');
+//   return result;
+// };
+
+// const r1 = add(2, 4);
+// console.log('r1: ', r1);
+
+// const fn = function (value) {
+//   console.log(1);
+
+//   console.log(2);
+
+//   if (value < 30) {
+//     return 'Меньше 5';
+//   }
+//   return 'Больше 5';
+// };
+
+// console.log('Результат ф-ции1:', fn(10));
+// console.log('Результат ф-ции2:', fn(1000));
+
+// const fnA = function () {
+//   console.log('Начала выполняться fnA');
+//   fnB();
+// };
+
+// const fnB = function () {
+//   console.log('Начала выполняться fnB');
+
+//   fnC();
+// };
+
+// const fnC = function () {
+//   console.log('Начала выполняться fnC');
+//   console.log(value);
+// };
+
+// fnA();
+// fnB();
+// fnC();
---
+++ module 2 less 4/02-calculate-total-price.js
@@ -0,0 +1,24 @@
+// const cart = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let total = 0;
+
+// for (const value of cart) {
+//   total += value;
+// }
+
+// console.log('Total: ', total);
+
+// const calculateTotalPrice = function (items) {
+//   console.log('items: ', items);
+
+//   let total = 0;
+
+//   for (const item of items) {
+//     total += item;
+//   }
+
+//   return total;
+// };
+
+// console.log(calculateTotalPrice([12, 85, 37, 4])); // 138
+// console.log(calculateTotalPrice([164, 48, 291])); // 503
+// console.log(calculateTotalPrice([412, 371, 94, 63, 176])); // 1116
