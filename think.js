c9d9a4c22dc682e1e414c7815a0b2d1a9be46f39
Nazar <nazarcorovol09@gmail.com>
2025-02-26 14:12:04 +0200

    практика

--- think.js
+++ think.js
@@ -103,3 +103,50 @@
 
 // Задание 11: Первая буква заглавная
 // Создай функцию capitalize, которая принимает строку и возвращает её с первой буквой в верхнем регистре, а остальные буквы оставляет как есть.
+
+// const capitalize = function (str) {
+//   return str[0].toUpperCase() + str.slice(1);
+// };
+
+// console.log(capitalize('hello'));
+// console.log(capitalize('world'));
+// console.log(capitalize('a'));
+
+// Задание 12: Сумма элементов массива
+// Напиши функцию sumArray, которая принимает массив чисел и возвращает их сумму.
+
+// const sumArray = function (items) {
+//   console.log('items: ', items);
+
+//   let total = 0;
+
+//   for (const item of items) {
+//     total += item;
+//   }
+
+//   return total;
+// };
+
+// console.log(sumArray([12, 85, 37, 4]));
+// console.log(sumArray([164, 48, 291]));
+// console.log(sumArray([412, 371, 94, 63, 176]));
+
+// Задание 13: Подсчет гласных
+// Напиши функцию countVowels, которая принимает строку и возвращает количество гласных букв в ней (считаем только "а", "е", "и", "о", "у", "ы", "э", "ю", "я" — в нижнем регистре).
+
+// const countVowels = function (a, b, c, d) {
+//     for (const element of object) {
+
+//     }
+// };
+
+// console.log(countVowels('a', 'b', 'c', 'd'));
+
+// Задание 14: Фильтр четных чисел
+// Создай функцию filterEven, которая принимает массив чисел и возвращает новый массив, содержащий только четные числа.
+
+const filterEven = function (a, b, c) {
+  return filterEven;
+};
+
+console.log(filterEven([2, 4, 6]));
